4715db6b7a545d74732d76a844a58f0c0c6cd5d4
Roman <cisinskijroman@gmail.com>
2025-01-27 16:03:16 +0200

    PEP8

--- main.py
+++ main.py
@@ -1,28 +1,38 @@
 num1 = int(input("Enter first num: "))
 num2 = int(input("Enter second num: "))
 
+
 def mult(num1, num2):
     print(f"result of multiply is {num1*num2}")
 
+
 mult(num1, num2)
+
 
 def suming(num1, num2):
     print(f"result of suming is {num1+num2}")
 
+
 suming(num1, num2)
+
 
 def raise_pow(num1, num2):
     print(f"result of suming is {pow(num1, num2)}")
 
+
 raise_pow(num1, num2)
+
 
 def subtraction(num1, num2):
     print(f"result of subtraction is {num1-num2}")
 
+
 subtraction(num1, num2)
 
+
 def module(num1, num2):
-    print(f"result of first`s num module is {abs(num1)} second`s num module is {abs(num2)}")
+    print(
+        f"result of first`s num module is {abs(num1)} second`s num module is {abs(num2)}")
+
 
 module(num1, num2)
-
